56967b599f450c08ce9e46892a7136a167168db1
Jorge Barcos <jorgebarcos88@gmail.com>
2019-09-19 19:22:50 -0300

    Creando el Header

--- src/App.js
+++ src/App.js
@@ -1,7 +1,12 @@
-import React from 'react';
+import React, { Fragment } from 'react';
+import Header from './components/Header';
 
 function App() {
-	return <h1>Hola</h1>;
+	return (
+		<Fragment>
+			<Header />;
+		</Fragment>
+	);
 }
 
 export default App;
