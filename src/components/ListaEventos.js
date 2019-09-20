80246d0cdf96849b807b870837d643f6fb33368d
Jorge Barcos <jorgebarcos88@gmail.com>
2019-09-20 17:55:42 -0300

    finalizando el proyecto

--- src/components/ListaEventos.js
+++ src/components/ListaEventos.js
@@ -7,7 +7,7 @@ const ListaEventos = () => {
 		<div className="uk-child-width-1-3@m" uk-grid="true">
 			<EventosConsumer>
 				{(value) => {
-					return value.eventos.map((evento) => <Evento />);
+					return value.eventos.map((evento) => <Evento key={evento.id} evento={evento} />);
 				}}
 			</EventosConsumer>
 		</div>
